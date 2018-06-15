6aa69688f478b675d9576ac4209b0f66f914c28e
Benjamin Hinchley <benjaminhinchley@gmail.com>
2018-06-15 12:46:30 +1000

    segmenter: refactor dictionary loader

--- segmenter.go
+++ segmenter.go
@@ -4,6 +4,7 @@ package sego
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"math"
 	"os"
@@ -13,9 +14,8 @@ import (
 	"unicode/utf8"
 )
 
-const (
-	minTokenFrequency = 2 // 仅从字典文件中读取大于等于此频率的分词
-)
+// Only read participles greater than or equal to this frequency from the dictionary file
+const minTokenFrequency = 2
 
 // 分词器结构体
 type Segmenter struct {
@@ -28,82 +28,95 @@ type jumper struct {
 	token       *Token
 }
 
-// 返回分词器使用的词典
+// Dictionary returns the dictionary
 func (seg *Segmenter) Dictionary() *Dictionary {
 	return seg.dict
 }
 
-// 从文件中载入词典
+// LoadDictionary loads a dictionary from a file
 //
-// 可以载入多个词典文件，文件名用","分隔，排在前面的词典优先载入分词，比如
-// 	"用户词典.txt,通用词典.txt"
-// 当一个分词既出现在用户词典也出现在通用词典中，则优先使用用户词典。
+// Multiple dictionary files can be loaded, with filenames separated by ",".
+// "User Dictionary.txt, Common Dictionary.txt"
+// When a participle appears in both the user dictionary and the general dictionary, the user dictionary is used preferentially.
 //
-// 词典的格式为（每个分词一行）：
-//	分词文本 频率 词性
-func (seg *Segmenter) LoadDictionary(files string) {
+// The format of the dictionary is (one line per participle):
+// Word segmentation text Frequency Part of speech
+func (seg *Segmenter) LoadDictionary(files string) error {
 	seg.dict = NewDictionary()
 	for _, file := range strings.Split(files, ",") {
 		log.Printf("载入sego词典 %s", file)
 		dictFile, err := os.Open(file)
 		defer dictFile.Close()
 		if err != nil {
-			log.Fatalf("无法载入字典文件 \"%s\" \n", file)
+			return fmt.Errorf("could not open %q: %v", file, err)
 		}
-
-		reader := bufio.NewReader(dictFile)
-		var text string
-		var freqText string
-		var frequency int
-		var pos string
-
-		// 逐行读入分词
-		for {
-			size, _ := fmt.Fscanln(reader, &text, &freqText, &pos)
-
-			if size == 0 {
-				// 文件结束
-				break
-			} else if size < 2 {
-				// 无效行
-				continue
-			} else if size == 2 {
-				// 没有词性标注时设为空字符串
-				pos = ""
-			}
-
-			// 解析词频
-			var err error
-			frequency, err = strconv.Atoi(freqText)
-			if err != nil {
-				continue
-			}
-
-			// 过滤频率太小的词
-			if frequency < minTokenFrequency {
-				continue
-			}
-
-			// 将分词添加到字典中
-			words := splitTextToWords([]byte(text))
-			token := Token{text: words, frequency: frequency, pos: pos}
-			seg.dict.addToken(token)
-		}
+		seg.tokenizeDictionary(dictFile)
 	}
+	seg.processDictionary()
 
-	// 计算每个分词的路径值，路径值含义见Token结构体的注释
+	return nil
+}
+
+// LoadDictionaryFromReader loads a dictionary from an io.Reader
+//
+// The format of the dictionary is (one line per participle):
+// Word segmentation text Frequency Part of speech
+func (seg *Segmenter) LoadDictionaryFromReader(r io.Reader) {
+	seg.tokenizeDictionary(r)
+	seg.processDictionary()
+}
+
+func (seg *Segmenter) tokenizeDictionary(r io.Reader) {
+	scanner := bufio.NewScanner(r)
+
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), " ")
+		if len(line) < 2 {
+			// invalid line
+			continue
+		}
+
+		text := line[0]
+		freqText := line[1]
+		pos := "" // part of speech tag
+		if len(line) > 2 {
+			pos = line[2]
+		}
+
+		// Analyze word frequency
+		frequency, err := strconv.Atoi(freqText)
+		if err != nil {
+			continue
+		}
+
+		// Filter words that are too small
+		if frequency < minTokenFrequency {
+			continue
+		}
+
+		// Add participles to the dictionary
+		words := splitTextToWords([]byte(text))
+		token := Token{text: words, frequency: frequency, pos: pos}
+		seg.dict.addToken(token)
+	}
+}
+
+func (seg *Segmenter) processDictionary() {
+	// Calculate the path value of each participle.
+	// For the meaning of the path value, see the annotation of the Token structure
 	logTotalFrequency := float32(math.Log2(float64(seg.dict.totalFrequency)))
 	for i := range seg.dict.tokens {
 		token := &seg.dict.tokens[i]
 		token.distance = logTotalFrequency - float32(math.Log2(float64(token.frequency)))
 	}
 
-	// 对每个分词进行细致划分，用于搜索引擎模式，该模式用法见Token结构体的注释。
+	// Make a careful division of each participle for the search engine pattern.
+	// For usage of this pattern, see Token structure comments.
 	for i := range seg.dict.tokens {
 		token := &seg.dict.tokens[i]
 		segments := seg.segmentWords(token.text, true)
 
-		// 计算需要添加的子分词数目
+		// Calculate the number of subparticiples that need to be added
 		numTokensToAdd := 0
 		for iToken := 0; iToken < len(segments); iToken++ {
 			if len(segments[iToken].token.text) > 0 {
@@ -112,7 +125,7 @@ func (seg *Segmenter) LoadDictionary(files string) {
 		}
 		token.segments = make([]*Segment, numTokensToAdd)
 
-		// 添加子分词
+		// Add child segmentation
 		iSegmentsToAdd := 0
 		for iToken := 0; iToken < len(segments); iToken++ {
 			if len(segments[iToken].token.text) > 0 {
@@ -121,8 +134,6 @@ func (seg *Segmenter) LoadDictionary(files string) {
 			}
 		}
 	}
-
-	log.Println("sego词典载入完毕")
 }
 
 // 对文本分词
@@ -135,7 +146,6 @@ func (seg *Segmenter) LoadDictionary(files string) {
 func (seg *Segmenter) Segment(bytes []byte) []Segment {
 	return seg.internalSegment(bytes, false)
 }
-
 
 func (seg *Segmenter) InternalSegment(bytes []byte, searchMode bool) []Segment {
 	return seg.internalSegment(bytes, searchMode)
